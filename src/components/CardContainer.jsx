03b63043204db8a1d05cc9006035a8c03b6c0abb
sinans <svrsinan06@gmail.com>
2023-07-12 13:36:07 +0300

    filtered

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,27 +1,47 @@
 import Team from "./Team"
-import {data} from "../helper/teamdata"
+import { data } from "../helper/teamdata"
 import Row from 'react-bootstrap/Row';
 import { Container } from "react-bootstrap";
+import Form from 'react-bootstrap/Form';
+import { useState } from "react";
 
 const CardContainer = () => {
+  const [search, setSearch] = useState("")
+
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
+ 
+const filteredData= data.filter((item)=>item.name.toLocaleLowerCase().includes(search.trim().toLocaleLowerCase()))
+ console.log(filteredData)
+
 
   return (
-    <Container>
-    <Row xs={1} md={2} lg={4}>
-    
-    {data.map((team,i)=>(
-      <div className="container col mb-3" key={i}>
-        <Team name={team.name} logo={team.logo} leagueLogo={team.leagueLogo} league={team.info[0]} estb={team.info[1]} champ={team.info[2]}/>
+    <>
+      <h3 className="text-white">UEFA Clubs</h3>
+      <div className="m-auto my-5 w-50 ">
+        <Form.Control onChange={handleChange} type="search" placeholder="Search Team..." className="w-100" />
       </div>
-    ))}
-    
 
-    </Row>
-    </Container>
+
+
+      <Container>
+        <Row xs={1} md={2} lg={3} >
+          {filteredData.map((team, i) => (
+            <div className="col m-auto mb-3justify-content-center g-3" key={i}>
+              <Team name={team.name} logo={team.logo} leagueLogo={team.leagueLogo} league={team.info[0]} estb={team.info[1]} champ={team.info[2]} />
+            </div>
+          ))}
+
+
+        </Row>
+      </Container>
+
+    </>
   )
-   
-    
-    
+
+
+
 }
 
 export default CardContainer
--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,28 +1,24 @@
 import Card from 'react-bootstrap/Card';
-import Image from 'react-bootstrap/Image';
+
 
 import ListGroup from 'react-bootstrap/ListGroup';
 
 const Team = ({name,logo,leagueLogo,league,estb,champ}) => {
   return (
-    <Card style={{ width: '16rem' }}>
-      <div className='logo'>
-        {/* <div style={{ width: '8rem', height:'8rem', marginTop:'1rem' }}>
-        <Image src={logo} fluid />
-      </div> */}
-      <Card.Img fluid style={{ width: '8rem', height:'8rem', marginTop:'1rem' }} variant="top" src={logo} />
+    <Card style={{ width: '16rem', minHeight:'35rem' }}>
+      <div className='logo m-auto'>
+       
+      <Card.Img style={{ width: '8rem', height:'8rem', marginTop:'1rem' }} variant="top" src={logo} />
       <Card.Body>
         <Card.Title className='text-primary'>{name}</Card.Title>
         
       </Card.Body>
       </div>
       <div className='info d-flex flex-column align-items-center mb-5'>
-        <div style={{ width: '4rem', height:'4rem', marginTop:'1rem' }}>
-        <Image src={leagueLogo}  fluid />
-      </div>
-      {/* <Card.Img style={{ width: '5rem', height:'5rem' ,marginTop:'1rem' }} variant="top" src={leagueLogo} /> */}
+        
+      <Card.Img style={{ maxWidth: '5rem', maxHeight:'5rem' ,marginTop:'1rem' }} variant="top" src={leagueLogo} />
       <Card.Body>
-      <ListGroup className='mt-3'>
+      <ListGroup className='mt-3 m-auto'>
       <ListGroup.Item> {league}</ListGroup.Item>
       <ListGroup.Item>{estb}</ListGroup.Item>
       <ListGroup.Item>{champ}</ListGroup.Item>
